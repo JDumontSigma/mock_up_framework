b931d8bf22744f88c7728d9b90ef63f5c8a1bae3
Jason Dumont <jason.dumont@sigma.se>
2017-07-19 13:14:46 +0100

    Fix routing on node app

--- server-tasks/routes/home.js
+++ server-tasks/routes/home.js
@@ -6,6 +6,18 @@ const express = require('express'),
 home.get('/', (req, res) => {
    res.render('index');
 });
+home.get('/index.html', (req, res) => {
+      res.render('index');
+});
+home.get('/home.html', (req, res) => {
+      res.render('home');
+});
+home.get('/prelim.html', (req, res) => {
+      res.render('prelim');
+});
+home.get('/study.html', (req, res) => {
+      res.render('study');
+});
 home.get('/error', (req, res) => {
    res.render('error');
 })
